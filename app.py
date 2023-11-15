86e5bc4fe3cea5f51b19858290aede3a8bf8c69c
luxa <jordibtje@gmail.com>
2023-11-15 04:41:04 +0000

    Added function to add multiple configs

--- app.py
+++ app.py
@@ -5,32 +5,32 @@ import os
 
 app = Flask(__name__)
 
-def process_provider_data(provider, form_data, provider_config):
+def process_provider_data(provider, form_data, provider_config, config_number):
     litellm_params = {}
     for fieldObj in provider_config.get('fields', []):
         field = list(fieldObj.keys())[0]  # Get the field name
         field_type = fieldObj[field]
 
-        if isinstance(field_type, dict) and field_type.get('type') == 'select':
+        form_field_name = f"{field}-{config_number}"
+        if field_type == 'checkbox':
+            litellm_params[field] = form_field_name in form_data
+        elif isinstance(field_type, dict) and field_type.get('type') == 'select':
             # Handle dropdown selections
-            litellm_params[field] = form_data.get(field)
-        elif field_type == 'checkbox':
-            litellm_params[field] = field in form_data
+            litellm_params[field] = form_data.get(form_field_name, provider_config.get('defaults', {}).get(field))
         else:
-            # For huggingface, skip adding user to litellm_params
-            if provider == "huggingface" and field == 'user':
-                continue
-            litellm_params[field] = form_data.get(field, provider_config.get('defaults', {}).get(field))
+            litellm_params[field] = form_data.get(form_field_name, provider_config.get('defaults', {}).get(field))
 
-    # Special handling for the huggingface provider
-    if provider == "huggingface":
-        user = form_data.get('user', '')
-        model = form_data.get('model', '')
-        litellm_params['model'] = f"huggingface/{user}/{model}" if user and model else model
-
+    # Apply default values if not present
     for key, value in provider_config.get('defaults', {}).items():
         if key not in litellm_params:
             litellm_params[key] = value
+
+    # Special handling for the huggingface provider
+    if provider == "huggingface":
+        user = form_data.get(f'user-{config_number}', '')
+        model = form_data.get(f'model-{config_number}', '')
+        litellm_params['model'] = f"huggingface/{user}/{model}" if user and model else model
+
     return litellm_params
 
 @app.route('/', methods=['GET', 'POST'])
@@ -40,22 +40,22 @@ def index():
 
     if request.method == 'POST':
         form_data = request.form
-        provider = form_data.get('providers')
-        model_name = form_data.get('model_name')
         show_yaml = 'show_yaml' in form_data
-        output_dir = form_data.get('output_dir', '')
+        config_count = int(form_data.get('config_count', 1))
 
-        provider_config = options['providers'].get(provider, {})
-        litellm_params = process_provider_data(provider, form_data, provider_config)
+        config_data = {"model_list": []}
 
-        config_data = {
-            "model_list": [
-                {
-                    "model_name": model_name,
-                    "litellm_params": litellm_params
-                }
-            ]
-        }
+        # Process each configuration
+        for i in range(1, config_count + 1):
+            provider = form_data.get(f'providers-{i}')
+            model_name = form_data.get(f'model_name-{i}')
+            provider_config = options['providers'].get(provider, {})
+            litellm_params = process_provider_data(provider, form_data, provider_config, i)
+
+            config_data["model_list"].append({
+                "model_name": model_name,
+                "litellm_params": litellm_params
+            })
 
         yaml_data = yaml.dump(config_data, default_flow_style=False, sort_keys=False)
 
@@ -63,6 +63,7 @@ def index():
             return jsonify({'yaml': yaml_data})
         else:
             file_name = 'config.yaml'
+            output_dir = form_data.get('output_dir', '')
             if output_dir:
                 if not os.path.exists(output_dir):
                     os.makedirs(output_dir)
